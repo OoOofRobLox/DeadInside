4d6f961b93b313891acaa2cb207ad5a844e446e6
OoOofRobLox <55408052+OoOofRobLox@users.noreply.github.com>
2021-06-18 23:38:07 +0300

    Add files via upload

---
+++ routes/user/startpage.py
@@ -0,0 +1,17 @@
+from flask import render_template, request, redirect, url_for, flash
+
+from app import app, db
+from model import Movie, Genre, Criteria
+from utils.decorator import has_authority
+from flask_login import login_required, current_user
+
+
+
+@app.route('/startpage', methods=['GET'])
+@login_required
+def startpage():
+    user=current_user
+    movies = Movie.query.order_by(Movie.name.asc()).all()
+    genres = Genre.query.all()
+    criterias = Criteria.query.all()
+    return render_template("user/startpage.html", movies=movies, genres=genres, criterias=criterias, user=user)
